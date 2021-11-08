850306060c34487b413884f52e0fb1408c2fd98b
Debian <debian@vps-dfed92b7.vps.ovh.net>
2021-11-08 19:54:54 +0000

    server ovh

--- Server-OVH/Nginx service/init-letsencrypt.sh
+++ Server-OVH/Nginx service/init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="checkpcs.com"
+domains=(checkpcs.com www.checkpcs.com)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="bauchau.louis@gmail.com" # Adding a valid address is strongly recommended
+email="louis.bauchau@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
