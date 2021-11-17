dd47a20a3e2fa9b1ea25cb1d88d6d9b87c3b4c04
Gabrielle Cruz <47248597+JhandarTheUnhallowed@users.noreply.github.com>
2021-11-18 00:31:49 +0100

    Python Script Modification
    Dans ce commit, les fonctions liées au base des données sont séparées dans une autre fichier, et il est lié à un fichier .env qui contient les coordonnées de la base des données. Les fonctions qui check les tableaux sont aussi changées en ajoutant les clés primaires et étrangères. Finalement, le dictionnaire qui contient les données récuperées par les tests est restructurée pour être plus propre.

--- Application/python/dbconnection.py
+++ Application/python/dbconnection.py
@@ -1,8 +1,129 @@
-import mariadb as mdb
 import sys
+import mariadb as mdb
+import os
+from dotenv import load_dotenv as env
+import time
 
-host = "localhost"
-user = "root"
-password = "Jh@ndar.506"
-port = 3307
-database = "python_data"
+### SETUP ENVIRONMENT VARIABLES ###
+env()
+
+### CONNECT TO DATABASE ###
+try:
+    my_db = mdb.connect(
+        port = int(os.environ.get("PORTDB")),
+        host = os.environ.get("HOST"),
+        user = os.environ.get("USER"),
+        password = os.environ.get("PASSWORDDB"),
+        database = os.environ.get("DATABASE")
+    )
+except mdb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+db_cursor = my_db.cursor()
+
+
+### FUNCTIONS TO CHECK TABLES ###
+def check_user_table():
+    db_cursor.execute("SHOW TABLES LIKE 'users';")
+    test = db_cursor.fetchall()
+    # print(test)
+    if len(test) == 0:
+        # print('Does not exist')
+        db_cursor.execute("CREATE TABLE users (id INT NOT NULL, username VARCHAR(45) NOT NULL, \
+        passwords VARCHAR(500) NOT NULL, email VARCHAR(60) NOT NULL, nom VARCHAR(45) NOT NULL, \
+        prenom VARCHAR(45) NOT NULL, PRIMARY KEY (id));")
+
+def check_basic_info_table():
+    db_cursor.execute("SHOW TABLES LIKE 'basic_info_test';")
+    test = db_cursor.fetchall()
+    if len(test) == 0:
+        #print('Does not exist')
+        db_cursor.execute("CREATE TABLE basic_info_test (testID INT NOT NULL AUTO_INCREMENT, userID INT NOT NULL, \
+        test_date VARCHAR(60) NOT NULL, user_name VARCHAR(60) NOT NULL, processor VARCHAR(60) NOT NULL, \
+        cpu_type VARCHAR(60) NOT NULL, PRIMARY KEY (testID), FOREIGN KEY (userID) REFERENCES users(id));")
+
+def check_battery_table():
+    db_cursor.execute("SHOW TABLES LIKE 'battery_test';")
+    test = db_cursor.fetchall()
+    if len(test) == 0:
+        # print('Does not exist')
+        db_cursor.execute("CREATE TABLE battery_test (testID INT NOT NULL AUTO_INCREMENT, userID INT NOT NULL, \
+        test_date VARCHAR(60) NOT NULL, battery_percent FLOAT(3,1) NOT NULL, PRIMARY KEY (testID), \
+        FOREIGN KEY (userID) REFERENCES users(id));")
+
+def check_cpu_table():
+    db_cursor.execute("SHOW TABLES LIKE 'cpu_test';")
+    test = db_cursor.fetchall()
+    if len(test) == 0:
+        #print('Does not exist')
+        db_cursor.execute("CREATE TABLE cpu_test (testID INT NOT NULL AUTO_INCREMENT, userID INT NOT NULL, \
+        test_date VARCHAR(60) NOT NULL, cpu_percent FLOAT(3,1) NOT NULL, PRIMARY KEY (testID), \
+        FOREIGN KEY (userID) REFERENCES users(id));")
+
+def check_ram_table():
+    db_cursor.execute("SHOW TABLES LIKE 'ram_test';")
+    test = db_cursor.fetchall()
+    if len(test) == 0:
+        #print('Does not exist')
+        db_cursor.execute("CREATE TABLE ram_test (testID INT NOT NULL AUTO_INCREMENT, userID INT NOT NULL, \
+        test_date VARCHAR(60) NOT NULL, total_virtual VARCHAR(12) NOT NULL, percent_virtual FLOAT(3,1) NOT NULL, \
+        total_swap VARCHAR(12) NOT NULL, percent_swap FLOAT(3,1) NOT NULL, PRIMARY KEY (testID), FOREIGN KEY (userID) REFERENCES users(id));")
+
+def check_storage_table():
+    db_cursor.execute("SHOW TABLES LIKE 'storage_test';")
+    test = db_cursor.fetchall()
+    if len(test) == 0:
+        # print('Does not exist')
+        db_cursor.execute("CREATE TABLE storage_test (testID INT NOT NULL AUTO_INCREMENT, userID INT NOT NULL, \
+        test_date VARCHAR(60) NOT NULL, total_storage VARCHAR(20) NOT NULL, used_storage VARCHAR(20) NOT NULL, \
+        PRIMARY KEY (testID), FOREIGN KEY (userID) REFERENCES users(id));")
+
+### FUNCTIONS TO SEND DATA ###
+def basic_info_test_to_db(userID, current_date, user, processor, cpu_type):
+    sql = "INSERT INTO basic_info_test (userID, test_date, user_name, processor, cpu_type) VALUES (%s, %s, %s, %s, %s)"
+    values = (userID, current_date, user, processor, cpu_type)
+    db_cursor.execute(sql, values)
+    my_db.commit()
+
+def battery_test_to_db(userID, current_date, percent):
+    sql = "INSERT INTO battery_test (userID, test_date, battery_percent) VALUES (%s, %s, %s)"
+    values = (userID, current_date, percent)
+    db_cursor.execute(sql, values)
+    my_db.commit()
+
+def cpu_test_to_db(userID, current_date, percent):
+    sql = "INSERT INTO cpu_test (userID, test_date, cpu_percent) VALUES (%s, %s, %s)"
+    values = (userID, current_date, percent)
+    db_cursor.execute(sql, values)
+    my_db.commit()
+
+def ram_test_to_db(userID, current_date, vram_total, vram_percent, swap_total, swap_percent):
+    sql = "INSERT INTO ram_test (userID, test_date, total_virtual, percent_virtual, total_swap, percent_swap) VALUES \
+                  (%s, %s, %s, %s, %s, %s)"
+    values = (userID, current_date, vram_total, vram_percent, swap_total, swap_percent)
+    db_cursor.execute(sql, values)
+    my_db.commit()
+
+def storage_test_to_db(userID, current_date, total_storage, used_storage):
+    time.sleep(5)
+    sql = "INSERT INTO storage_test (userID, test_date, total_storage, used_storage) VALUES (%s, %s, %s, %s)"
+    values = (userID, current_date, total_storage, used_storage)
+    # turn into list
+    db_cursor.execute(sql, values)
+    my_db.commit()
+
+### LOAD DATABASE ###
+def load_db():
+    check_user_table()
+    check_basic_info_table()
+    check_cpu_table()
+    check_ram_table()
+    check_battery_table()
+    check_storage_table()
+
+if __name__ == '__main__':
+    pass
+
+
+
--- Application/python/extra.py
+++ Application/python/extra.py
@@ -5,35 +5,19 @@ import cpuinfo
 import GPUtil
 from datetime import datetime
 from threading import *
-import mariadb as mdb
-import sys
 import time
 from pyspectator.processor import Cpu # temperature
 import dbconnection as dbc
 import math
-# import wmi
-
-### LOGIN TO MYSQL DATABASE###
-try:
-    my_db = mdb.connect(
-        host=dbc.host,
-        user=dbc.user,
-        password=dbc.password,
-        port=dbc.port,
-        database=dbc.database
-    )
-except mdb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
-db_cursor = my_db.cursor()
-
-u_name = "Jacques"
-now_date = datetime.today()
+import os
+from dotenv import load_dotenv as env
 
 IS_RUNNING = True
 computer_data = {}
 BUTTON_TOGGLE = False
+
+USERID = int(os.environ.get("USERID"))
+CURRENT_DATE = datetime.now()
 
 # https://www.thepythoncode.com/article/get-hardware-system-information-python
 def get_size(bytes, suffix='B'):
@@ -43,78 +27,25 @@ def get_size(bytes, suffix='B'):
             return f"{bytes:.2f}{unit}{suffix}"
         bytes /= factor
 
-
-def cpu_test():
-   while IS_RUNNING:
-       """print('--------------------------')
-          print('------- CPU Test ---------')
-          print('--------------------------')"""
-       i = 0
-       while True:
-           i = i + 1
-           cpu = psutil.cpu_percent(interval=1)
-           # print(cpu)
-           # cpu_proc = mproc(target=cpu_test_multiproc, args=[cpu])
-           # change to thread => daemon
-           # cpu_proc.start()
-           # cpu_proc.join()
-           if cpu > 60 or i == 10:
-               # print("FINI")
-               break
-       computer_data['cpu_percent'] = cpu
-       time.sleep(2)
-
-
-def cpu_test_to_db():
-    time.sleep(5)
-    sql = "INSERT INTO cpu_test (userID, test_date, cpu_percent) VALUES (%s, %s, %s)"
-    values = (u_name, now_date, computer_data['cpu_percent'])
-    # turn into list
-    db_cursor.execute(sql, values)
-    my_db.commit()
-
-
 def convert_time(seconds):
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     return "%d:%02d:%02d" % (hours, minutes, seconds)
 
-
-def battery_test():
-    while IS_RUNNING:
-        """print('--------------------------')
-            print('----- Battery Test ------')
-            print('--------------------------')"""
-        battery = psutil.sensors_battery()
-        computer_data['battery'] = battery.percent
-
-        """print("Battery percentage : ", battery.percent)
-        print("Power plugged in : ", battery.power_plugged)
-
-        # converting seconds to hh:mm:ss
-        print("Battery left : ", convert_time(battery.secsleft))"""
-        time.sleep(60)
-
-def battery_test_to_db():
-    time.sleep(5)
-    sql = "INSERT INTO battery_test (userID, test_date, battery_percent) VALUES (%s, %s, %s)"
-    values = (u_name, now_date, computer_data['battery'])
-    # turn into list
-    db_cursor.execute(sql, values)
-    my_db.commit()
-
+### TESTS ###
 def basic_info_test():
+    computer_data['info'] = {}
     while IS_RUNNING:
         """print('--------------------------')
         print('------- info Test ---------')
         print('--------------------------')"""
         user = psutil.users()
         # print(f"user is : {user[0].name}")
-        computer_data['user'] = user[0].name
+        computer_data['info']['user'] = user[0].name
         # print(f"User: {user[0].name}")
-        computer_data['processor'] = platform.processor()
+        computer_data['info']['processor'] = platform.processor()
         # print(f"the processor is : {platform.processor()}")
-        computer_data['cpu_type'] = cpuinfo.get_cpu_info()['brand_raw']
+        computer_data['info']['cpu_type'] = cpuinfo.get_cpu_info()['brand_raw']
         # print(f"CPU is : {cpu}")
         # test1 = platform.machine()
         # print(test1)
@@ -122,19 +53,32 @@ def basic_info_test():
         # print(f"the processor is : {lol1.brand_raw}")
         time.sleep(300)
 
-def basic_info_test_to_db():
-    time.sleep(5)
-    sql = "INSERT INTO basic_info_test (userID, test_date, user_name, processor, cpu_type) VALUES (%s, %s, %s, %s, %s)"
-    values = (u_name, now_date, computer_data['user'], computer_data['processor'], computer_data['cpu_type'])
-    #turn into list
-    db_cursor.execute(sql, values)
-    my_db.commit()
+def battery_test():
+    computer_data['battery'] = {}
+    while IS_RUNNING:
+        """print('--------------------------')
+            print('----- Battery Test ------')
+            print('--------------------------')"""
+        battery = psutil.sensors_battery()
+        computer_data['battery']['percent'] = battery.percent
+        time.sleep(0.5)
+
+def cpu_test():
+    computer_data['cpu'] = {}
+    while IS_RUNNING:
+        i = 0
+        while True:
+            i = i + 1
+            cpu = psutil.cpu_percent(interval=1)
+            if cpu > 60 or i == 10:
+                # print("FINI")
+                break
+        computer_data['cpu']['percent'] = cpu
+        time.sleep(0.5)
 
 def ram_test():
+    computer_data['ram'] = {}
     while IS_RUNNING:
-        computer_data['virtual_ram'] = {}
-        computer_data['swap_ram'] = {}
-
         svmem = psutil.virtual_memory()
         """print('--------------------------')
         print('------- RAM Test ---------')
@@ -143,8 +87,8 @@ def ram_test():
         print(f" Available: {get_size(svmem.available)}")
         print(f" Used: {get_size(svmem.used)}")
         print(f" Percentage: {get_size(svmem.percent)} %")"""
-        computer_data['virtual_ram']['total'] = get_size(svmem.total)
-        computer_data['virtual_ram']['percent'] = svmem.percent
+        computer_data['ram']['vram_total'] = get_size(svmem.total)
+        computer_data['ram']['vram_percent'] = svmem.percent
 
         swap = psutil.swap_memory()
         """print('\nSwap Partition: ')
@@ -152,29 +96,17 @@ def ram_test():
         print(f" Free: {get_size(swap.free)}")
         print(f" Used: {get_size(swap.used)}")
         print(f" Percentage: {get_size(swap.percent)} %")"""
-        computer_data['swap_ram']['total'] = get_size(swap.total)
-        computer_data['swap_ram']['percent'] = swap.percent
-
-        time.sleep(5)
-
-def ram_test_to_db():
-    time.sleep(5)
-    sql = "INSERT INTO ram_test (userID, test_date, total_virtual, percent_virtual, total_swap, percent_swap) VALUES \
-                  (%s, %s, %s, %s, %s, %s)"
-    values = (u_name, now_date, computer_data['virtual_ram']['total'],
-              computer_data['virtual_ram']['percent'], computer_data['swap_ram']['total'],
-              computer_data['swap_ram']['percent'])
-    # turn into list
-    db_cursor.execute(sql, values)
-    my_db.commit()
+        computer_data['ram']['swap_total'] = get_size(swap.total)
+        computer_data['ram']['swap_percent'] = swap.percent
+        time.sleep(0.5)
 
 def storage_test():
+    computer_data['storage_disk'] = {}
     while IS_RUNNING:
-        computer_data['storage_disk'] = {}
         partitions = psutil.disk_partitions()
         # disk_nums, total storage, total used
         # print(len(partitions))
-        computer_data['storage_disk']['number'] = len(partitions)
+        #computer_data['storage_disk']['partitions'] = len(partitions)
         total_storage_size = 0
         total_storage_used = 0
 
@@ -182,25 +114,16 @@ def storage_test():
             try:
                 partition_usage = psutil.disk_usage(partition.mountpoint)
             except PermissionError:
-                # this can be catched due to the disk that
+                # this can be caught due to the disk that
                 # isn't ready
                 continue
             total_storage_size += partition_usage.total
             total_storage_used += partition_usage.used
 
-        computer_data['storage_disk']['total'] = get_size(total_storage_size)
-        computer_data['storage_disk']['used'] = get_size(total_storage_used)
+        computer_data['storage_disk']['total_storage'] = get_size(total_storage_size)
+        computer_data['storage_disk']['used_storage'] = get_size(total_storage_used)
 
-        time.sleep(90)
-
-def storage_test_to_db():
-    time.sleep(5)
-    sql = "INSERT INTO storage_test (userID, test_date, total_storage, used_storage) VALUES (%s, %s, %s, %s)"
-    values = (u_name, now_date, computer_data['storage_disk']['total'],
-              computer_data['storage_disk']['used'])
-    # turn into list
-    db_cursor.execute(sql, values)
-    my_db.commit()
+        time.sleep(60)
 
 #TO DO
 def gpu_test():
@@ -255,82 +178,38 @@ def display_data():
     while IS_RUNNING:
         print('Waiting for data...')
         time.sleep(15)
-
-        send_data_to_db()
+        #print(computer_data)
 
         print('=====OVERVIEW=====')
         print(f'Time: {datetime.today()}')
-        print(f"User: {computer_data['user']}")
-        print(f"CPU: {computer_data['cpu_type']}")
-        print(f"Processor: {computer_data['processor']}")
-        print(f"Battery Percentage: {computer_data['battery']}%")
-        print(f"CPU Percentage: {computer_data['cpu_percent']}%")
+        print(f"User: {computer_data['info']['user']}")
+        print(f"CPU: {computer_data['info']['cpu_type']}")
+        print(f"Processor: {computer_data['info']['processor']}")
+        print(f"Battery Percentage: {computer_data['battery']['percent']}%")
+        print(f"CPU Percentage: {computer_data['cpu']['percent']}%")
         print("\n")
         print(f"Number of Disk Partitions: {computer_data['storage_disk']['number']}")
-        print(f"Total Storage Space: {computer_data['storage_disk']['total']}")
-        print(f"Used Storage Space: {computer_data['storage_disk']['used']}")
+        print(f"Total Storage Space: {computer_data['storage_disk']['total_storage']}")
+        print(f"Used Storage Space: {computer_data['storage_disk']['used_storage']}")
         print("\n")
-        print(f"RAM - Total Virtual Memory: {computer_data['virtual_ram']['total']}")
-        print(f"RAM - Virtual Memory Percentage: {computer_data['virtual_ram']['percent']}%")
-        print(f"RAM - Total Swap Memory: {computer_data['swap_ram']['total']}")
-        print(f"RAM - Swap Memory Percentage: {computer_data['swap_ram']['percent']}%")
+        print(f"RAM - Total Virtual Memory: {computer_data['ram']['vram_total']}")
+        print(f"RAM - Virtual Memory Percentage: {computer_data['ram']['vram_percent']}%")
+        print(f"RAM - Total Swap Memory: {computer_data['ram']['swap_total']}")
+        print(f"RAM - Swap Memory Percentage: {computer_data['ram']['swap_percent']}%")
         print("\n")
         #print(f"Network Download Speed: {computer_data['speedtest']['download']} Mbits/s")
         #print(f"Network Upload Speed: {computer_data['speedtest']['upload']} Mbits/s")
         #print(f"Network Ping: {computer_data['speedtest']['ping']} ms")
 
-
-def check_cpu_table():
-    db_cursor.execute("SHOW TABLES LIKE 'cpu_test';")
-    test = db_cursor.fetchall()
-    #print(test)
-    if len(test) == 0:
-        #print('Does not exist')
-        db_cursor.execute("CREATE TABLE cpu_test (userID VARCHAR(60), test_date VARCHAR(60), cpu_percent INT)")
-
-
-def check_ram_table():
-    db_cursor.execute("SHOW TABLES LIKE 'ram_test';")
-    test = db_cursor.fetchall()
-    #print(test)
-    if len(test) == 0:
-        #print('Does not exist')
-        db_cursor.execute("CREATE TABLE ram_test (userID VARCHAR(60), test_date VARCHAR(60), \
-        total_virtual VARCHAR(12), percent_virtual VARCHAR(12), total_swap VARCHAR(12), percent_swap VARCHAR(12))")
-
-def check_basic_info_table():
-    db_cursor.execute("SHOW TABLES LIKE 'basic_info_test';")
-    test = db_cursor.fetchall()
-    #print(test)
-    if len(test) == 0:
-        #print('Does not exist')
-        db_cursor.execute("CREATE TABLE basic_info_test (userID VARCHAR(60), test_date VARCHAR(60), \
-        user_name VARCHAR(60), processor VARCHAR(60), cpu_type VARCHAR(60))")
-
-def check_battery_table():
-    db_cursor.execute("SHOW TABLES LIKE 'battery_test';")
-    test = db_cursor.fetchall()
-    # print(test)
-    if len(test) == 0:
-        # print('Does not exist')
-        db_cursor.execute("CREATE TABLE battery_test (userID VARCHAR(60), test_date VARCHAR(60), \
-            battery_percent VARCHAR(10))")
-
-def check_storage_table():
-    db_cursor.execute("SHOW TABLES LIKE 'storage_test';")
-    test = db_cursor.fetchall()
-    # print(test)
-    if len(test) == 0:
-        # print('Does not exist')
-        db_cursor.execute("CREATE TABLE storage_test (userID VARCHAR(60), test_date VARCHAR(60), \
-            total_storage VARCHAR(20), used_storage VARCHAR(20))")
-
-def load_db():
-    check_basic_info_table()
-    check_cpu_table()
-    check_ram_table()
-    check_battery_table()
-    check_storage_table()
+def send_data():
+    while IS_RUNNING:
+        time.sleep(12)
+        dbc.basic_info_test_to_db(USERID, CURRENT_DATE, **computer_data['info'])
+        dbc.battery_test_to_db(USERID, CURRENT_DATE, **computer_data['battery'])
+        dbc.cpu_test_to_db(USERID, CURRENT_DATE, **computer_data['cpu'])
+        dbc.ram_test_to_db(USERID, CURRENT_DATE, **computer_data['ram'])
+        dbc.storage_test_to_db(USERID, CURRENT_DATE, **computer_data['storage_disk'])
+        print('Data successfully sent!')
 
 def run_tests():
     IS_RUNNING = True
@@ -354,7 +233,8 @@ def run_tests():
     #gpu_thread
     #network_thread
     #temperature_thread
-    display_thread = Thread(target=display_data, daemon=True)
+    #display_thread = Thread(target=display_data, daemon=True)
+    send_data_thread = Thread(target=send_data, daemon=True)
 
     ### initialize daemons
     ### once tkinter button is linked, we can toggle start and stop
@@ -367,7 +247,8 @@ def run_tests():
     #gpu_thread.start()
     #network_thread.start()
     #temperature_thread.start()
-    display_thread.start()
+    #display_thread.start()
+    send_data_thread.start()
 
     while IS_RUNNING:
         pass
@@ -375,15 +256,8 @@ def run_tests():
         if BUTTON_TOGGLE:
             IS_RUNNING = False
 
-def send_data_to_db():
-    basic_info_test_to_db()
-    cpu_test_to_db()
-    battery_test_to_db()
-    ram_test_to_db()
-    storage_test_to_db()
-
 if __name__ == "__main__":
-    load_db()
+    dbc.load_db()
     run_tests()
 
 
