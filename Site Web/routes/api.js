5dc7792e7c32ce796e7cecf7699c71b9c61e598c
louisbau <bauchau.louis@gmail.com>
2021-12-20 14:02:02 +0100

    supression commentaire

--- Site Web/routes/api.js
+++ Site Web/routes/api.js
@@ -14,68 +14,6 @@ router.get('/pcs', verifyJWT, async function (req, res) {
   }
 })
 
-/*
-
-router.get('/:idpc/dashboard', verifyJWT, async function (req, res) {
-  const id = req.params.idpc
-
-  try {
-    const sqlQuery = `SELECT percent_virtual FROM ram Where id = (SELECT MAX(id) FROM ram WHERE idPc = ?);
-                          SELECT cpu_percent FROM cpu Where id = (SELECT MAX(id) FROM cpu WHERE idPc = ?);
-                          SELECT battery_percent FROM battery Where id = (SELECT MAX(id) FROM battery WHERE idPc = ?);
-                          SELECT total_storage, used_storage FROM storage Where id = (SELECT MAX(id) FROM storage WHERE idPc = ?);`
-    const rows = await pool.query(sqlQuery, [id, id, id, id])
-    res.status(200).json(rows)
-  } catch (error) {
-    res.status(400).send(error.message)
-  }
-})
-router.get('/:idpc/battery', verifyJWT, async function (req, res) {
-  const id = req.params.idpc
-
-  try {
-    const sqlQuery = 'SELECT test_date, battery_percent FROM battery Where idPc=? ORDER BY STR_TO_DATE(test_date, "%d/%m/%Y") ASC'
-    const rows = await pool.query(sqlQuery, id)
-    res.status(200).json(rows)
-  } catch (error) {
-    res.status(400).send(error.message)
-  }
-})
-router.get('/:idpc/cpu', verifyJWT, async function (req, res) {
-  const id = req.params.idpc
-  try {
-    const sqlQuery = 'SELECT test_date, cpu_percent FROM cpu Where idPc=? ORDER BY STR_TO_DATE(test_date, "%d/%m/%Y") ASC'
-    const rows = await pool.query(sqlQuery, id)
-    res.status(200).json(rows)
-  } catch (error) {
-    res.status(400).send(error.message)
-  }
-})
-router.get('/:idpc/storage', verifyJWT, async function (req, res) {
-  const id = req.params.idpc
-
-  try {
-    const sqlQuery = 'SELECT test_date, total_storage, used_storage FROM storage Where idPc=? ORDER BY STR_TO_DATE(test_date, "%d/%m/%Y") ASC'
-    const rows = await pool.query(sqlQuery, id)
-    res.status(200).json(rows)
-  } catch (error) {
-    res.status(400).send(error.message)
-  }
-})
-
-router.get('/:idpc/ram', verifyJWT, async function (req, res) {
-  const id = req.params.idpc
-
-  try {
-    const sqlQuery = 'SELECT test_date, total_virtual, percent_virtual, total_swap FROM ram Where idPc=? ORDER BY STR_TO_DATE(test_date, "%d/%m/%Y") ASC'
-    const rows = await pool.query(sqlQuery, id)
-    res.status(200).json(rows)
-  } catch (error) {
-    res.status(400).send(error.message)
-  }
-})
-*/
-
 router.get('/dashboard', verifyJWT, async function (req, res) {
   try {
     const sqlQuery = `SELECT pcs.idPc, ram.percent_virtual, Max(ram.id) FROM pcs INNER JOIN ram ON pcs.idPc=ram.idPc WHERE pcs.idUser=? GROUP BY pcs.idPc;
